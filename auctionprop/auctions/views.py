852ad1361bf33e543159c89ae0c890542764b194
Rachit <rachitkum123456@gmail.com>
2024-03-22 22:50:59 +0530

    ...

--- auctionprop/auctions/views.py
+++ auctionprop/auctions/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'auctions/index.html')
+def login(request):
+    return render(request, 'auctions/login.html')
+def register(request):
+    return render(request, 'auctions/register.html')
