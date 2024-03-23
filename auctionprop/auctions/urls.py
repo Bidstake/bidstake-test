8640763d53b3a66fad241f8cc4caa6acefb54d09
Rachit <rachitkum123456@gmail.com>
2024-03-23 23:26:58 +0530

    user reg

--- auctionprop/auctions/urls.py
+++ auctionprop/auctions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("login", views.login, name="login"),
-     path("register", views.register, name="register"),
+    path('home', views.home, name='home'),
+    path("login", views.user_login, name="login"),
+    path("register", views.register, name="register"),
 ]
