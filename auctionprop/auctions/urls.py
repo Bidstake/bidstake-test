773edcd4ac17a9ed3071e1e6e1fc7c0ff9832843
Rachit <rachitkum123456@gmail.com>
2024-04-20 20:44:48 +0530

    ss

--- auctionprop/auctions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
-    path("login", views.user_login, name="login"),
-    path("register", views.register, name="register"),
-]
