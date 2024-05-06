f16fdd7bb552c13b998d613ae0783626ddf529f7
ahmedatk <ahmedzaweel@gmail.com>
2024-05-06 22:19:31 +0530

    test

--- auctionprop/settings.py
+++ auctionprop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^myg#pb6qz6pe@zzw+wct)&h^*#$@wcl+q34xh8)yzw05sc1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.201.132.108"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
